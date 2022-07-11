6b09af6fb91c703a073a8e3c36c25646a56f3f56
Dvauns <danilvusenko3@gmail.com>
2022-07-11 14:17:13 +0000

    Upload New File

---
+++ qemu.sh
@@ -0,0 +1,29 @@
+#!bin/bash
+
+clear
+
+echo "     __________"
+echo "    |   ____   | EMU"
+echo "    |  |    |  |"
+echo "    |  |    |  |"
+echo "    |  |____|  |"
+echo "    |_______   |"
+echo "            |  |"
+echo "    V1-0    |__|"
+echo ""
+echo "(1) Start Qemu"
+echo "(2) Settings"
+echo "(3) About US"
+echo "(X) Exit"
+read doing
+
+case $doing in
+"1") bash $HOME/Qemu/Data/start.sh
+;;
+"2") nano $HOME/Qemu/Data/start.sh
+;;
+"3") bash $HOME/Qemu/Data/about.sh
+;;
+"x") exit 0
+;;
+esac
